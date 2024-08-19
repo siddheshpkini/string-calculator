3623bec7959b4733a4d6f28b03fcb640cc3199e6
siddheshpkini <siddheshpkini@gmail.com>
2024-08-19 20:30:33 +0530

    removed unwanted items from home page and unused failing test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('string-calculator');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('string-calculator app is running!');
-  });
 });
